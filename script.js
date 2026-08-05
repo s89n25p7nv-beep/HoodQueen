11b2680d9c346623ffa23207a9cb7c11d1ffcc31
s89n25p7nv-beep <s89n25p7nv@privaterelay.appleid.com>
2026-08-05 00:49:21 -0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,5 @@
+console.log("Welcome to Hood Queen 👑");
+
+document.querySelector("button").addEventListener("click", function() {
+    alert("Welcome to Hood Queen. The movement begins now 👑");
+});
