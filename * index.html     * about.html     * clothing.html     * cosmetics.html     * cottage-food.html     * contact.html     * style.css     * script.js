b69642a09710fefd7debb18beed960d845ac5795
s89n25p7nv-beep <s89n25p7nv@privaterelay.appleid.com>
2026-08-04 22:37:15 -0700

    Create * index.html     * about.html     * clothing.html     * cosmetics.html     * cottage-food.html     * contact.html     * style.css     * script.js

---
+++ * index.html     * about.html     * clothing.html     * cosmetics.html     * cottage-food.html     * contact.html     * style.css     * script.js
@@ -0,0 +1,9 @@
+    * index.html
+    * about.html
+    * clothing.html
+    * cosmetics.html
+    * cottage-food.html
+    * contact.html
+    * style.css
+    * script.js
+    * your images folder
