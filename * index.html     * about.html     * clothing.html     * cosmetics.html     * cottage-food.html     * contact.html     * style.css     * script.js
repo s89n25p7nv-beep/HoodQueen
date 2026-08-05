126fbebb03e98caaded34e29816f264a77df0dd3
s89n25p7nv-beep <s89n25p7nv@privaterelay.appleid.com>
2026-08-05 00:05:15 -0700

    Delete * index.html     * about.html     * clothing.html     * cosmetics.html     * cottage-food.html     * contact.html     * style.css     * script.js

--- * index.html     * about.html     * clothing.html     * cosmetics.html     * cottage-food.html     * contact.html     * style.css     * script.js
+++
@@ -1,9 +0,0 @@
-    * index.html
-    * about.html
-    * clothing.html
-    * cosmetics.html
-    * cottage-food.html
-    * contact.html
-    * style.css
-    * script.js
-    * your images folder
